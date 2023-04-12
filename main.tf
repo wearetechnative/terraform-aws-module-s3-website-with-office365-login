cb343779ef8e37ca30b0d26793e318441df7b45d
Pim Snel <post@pimsnel.com>
2023-04-13 00:30:56 +0200

    use later version of static-website-cognito-authw

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "cognito_s3_website" {
 
-  source = "github.com/wearetechnative/terraform-aws-module-static-website-cognito-auth?ref=v0.3.0"
+  source = "github.com/wearetechnative/terraform-aws-module-static-website-cognito-auth?ref=v0.3.1"
 
   name                            = "website-${var.subdomain}-${replace(var.domain, ".", "-")}"
   domain                          = "${var.subdomain}.${var.domain}"
